f6763765c5a8df44a8d5f87dae8d78bc2b71c74e
srajah.shakila@gmail.com <srajah.shakila@gmail.com>
2016-07-14 21:54:36 +0530

    After releasing the tag

--- src/test/java/org/wso2/carbon/connector/integration/test/disqus/DisqusConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/disqus/DisqusConnectorIntegrationTest.java
@@ -46,7 +46,7 @@ public class DisqusConnectorIntegrationTest extends ConnectorIntegrationTestBase
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("disqus-connector-1.0.1");
+        init("disqus-connector-1.0.2-SNAPSHOT");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
     }
